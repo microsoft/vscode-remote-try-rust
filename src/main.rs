bb54e82f8cc364759eb90a544ea72e250d1e10ad
NewGyu <newgyu@kinoboku.net>
2021-06-22 14:48:49 +0000

    ADD: generating secret number

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
-
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the 番号!");
+
+    let secret_num = rand::thread_rng().gen_range(1..101);
+    println!("The secret number is: {}", secret_num);
     println!("Please input your guess.");
 
     let mut guess = String::new();
