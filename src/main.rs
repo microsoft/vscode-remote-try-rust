6e6270b07260edd3c080a8e188c01101dc37436f
NewGyu <newgyu@kinoboku.net>
2021-07-05 14:59:43 +0000

    引数でチュートリアル実行を指定できるように

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,22 @@
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License. See https://go.microsoft.com/fwlink/?linkid=2090316 for license information.
  *-------------------------------------------------------------------------------------------------------------*/
+use std::env;
+use std::process;
 
 mod tutorial;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        process::exit(1);
+    }
+
+    match args[1].as_str() {
+        "guess" => tutorial::guess_game::start(),
+        "ownership" => tutorial::ownership::start(),
     //    tutorial::guess_game::start();
     tutorial::ownership::start();
+        _ => process::exit(2),
+    }
 }
